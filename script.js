28addbd2d62f2ff7ef2ab8163214704dda4b3f5d
Pivanta <bas677@icloud.com>
2021-02-23 18:27:18 +0000

    Completed

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const img = document.getElementsByTagName("IMG");
-const ref = document.getElementsByTagName('A');
-const hEl = document.getElementsByTagName('H1');
+// Assign variables
+const imgTags = document.getElementsByTagName("IMG");
+const aTags = document.getElementsByTagName('A');
+const h1El = document.getElementsByTagName('H1');
 const siteEl = document.querySelector('.sites');
 const mainEl = document.getElementById('main');
 
-mainEl.style.textAlign = 'center';
-// hEl[0].style.textAlign = 'center';
-hEl[0].setAttribute('style', 'color: blue'); // does not overwrite other css values like in line 8, to overcome this you need to place line 8 after this line.
-hEl[0].style.textAlign = 'center'; // line 8
+// Array with a list of website addresses
+const randomSites = ['https://www.google.com', 'https://www.w3schools.com', 'https://developer.mozilla.org', 'https://www.github.com',  'https://www.freecodecamp.org']
+
+// Get random number with max number being the length of the randomSites array
+const randomNumber = Math.trunc(Math.random() * randomSites.length);
+console.log(randomNumber);
+// Style the body
 document.body.setAttribute('style', 'background-color: 	#F0F8FF');
 
+// Align all images in the center of the page
+mainEl.style.textAlign = 'center';
+// H4 styling
+h1El[0].setAttribute('style', 'text-align: center; color: blue');
+
+// Function to display images and alt tags
   function setPaths() {
-    for (let i = 0; i < ref.length; i++) {
-        ref[i].href = 'https://www.google.com';
-        img[i].src = `images/image_${i+1}.jpg`;
-        img[i].alt = `image_${i+1}`;
+    for (let i = 0; i < aTags.length; i++) {
+        aTags[i].href = 'https://www.google.com';
+        imgTags[i].src = `images/image_${i+1}.jpg`;
+        imgTags[i].alt = `image_${i+1}`;
     }
   }
   setPaths();
+
+  // Randomly set the `href` for site 1 from randomSites array 
+  aTags[0].href = randomSites[randomNumber];
+ 
